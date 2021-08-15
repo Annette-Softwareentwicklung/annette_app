533612d07aad028b6b8050d4276b492563db7343
jan2210 <jan.wermeckes@gmx.de>
2021-08-15 11:27:51 +0200

    Minor bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dismissKeyboard.dart';
 import 'introductionScreen.dart';
