c3dbb31603e10e0ec31f0a4b21ca2145c8044151
jan2210 <jan.wermeckes@gmx.de>
2021-08-22 18:20:20 +0200

    Version updated

--- lib/settingsTab.dart
+++ lib/settingsTab.dart
@@ -36,7 +36,7 @@ class SettingsTab extends StatelessWidget {
     showAboutDialog(
       context: context,
       applicationName: 'Annette App',
-      applicationVersion: 'Version 3.1.0+1',
+      applicationVersion: 'Version 3.1.0+4',
       applicationIcon: Container(
           height: 70,
           alignment: Alignment.center,
