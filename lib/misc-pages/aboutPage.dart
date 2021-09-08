9fe55d4df25bd560cd1226d9745f17af2312fb97
jan2210 <jan.wermeckes@gmx.de>
2021-09-08 11:25:04 +0200

    AboutPage updated

--- lib/misc-pages/aboutPage.dart
+++ lib/misc-pages/aboutPage.dart
@@ -9,14 +9,12 @@ import 'package:get_storage/get_storage.dart';
 import 'package:yaml/yaml.dart';
 
 class AboutPage extends StatelessWidget {
-
   static double sectionMargin = 20;
 
   DateTime? getTimetableVersion() {
     try {
       return DateTime.parse(GetStorage().read('timetableVersion'));
     } catch (e) {
-
       print(e);
       return null;
     }
@@ -27,93 +25,125 @@ class AboutPage extends StatelessWidget {
     return FutureBuilder(
       future: rootBundle.loadString("pubspec.yaml"),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-
         String appName = "Annette App";
         String version = "0.0.0";
         DateTime? timeTableVersion = getTimetableVersion();
-        YamlList developers;
-        String developerString = "";
-
+        List<dynamic> developers = ['---'];
 
         if (snapshot.hasData) {
           Map yamlData = loadYaml(snapshot.data);
           version = yamlData["version"];
-          developers = yamlData["authors"];
-          for (String developer in developers) {
-            developerString += "\n- $developer";
-
-          }
+          developers = yamlData["authors"].toList();
         }
-
-
 
         return new DefaultScaffold(
             title: "Über diese App",
             content: Container(
-                padding: EdgeInsets.symmetric(horizontal: sectionMargin, vertical: Design.standardPagePadding),
-                color: Colors.white,
-                child: ListView(
-                  scrollDirection: Axis.vertical,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+              alignment: Alignment.topCenter,
+                padding: EdgeInsets.symmetric(
+                    horizontal: sectionMargin,
+                    vertical: Design.standardPagePadding),
+                child: Scrollbar(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      //scrollDirection: Axis.vertical,
                       children: [
-                        Image(
-                          width: 100,
-                          height: 100,
-                          image: AssetImage(assetPaths.iconImagePath)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(5),
+                              padding: EdgeInsets.all(5),
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                boxShadow: (Theme.of(context).brightness ==
+                                        Brightness.dark)
+                                    ? null
+                                    : [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.15),
+                                          spreadRadius: 2,
+                                          blurRadius: 5,
+                                          offset: Offset(
+                                              0, 3), // changes position of shadow
+                                        ),
+                                      ],
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(13),
+                                shape: BoxShape.rectangle,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey, width: 1),
+                                  borderRadius: BorderRadius.circular(10),
+                                  shape: BoxShape.rectangle,
+                                  image: DecorationImage(
+                                      image: AssetImage(assetPaths.iconImagePath),
+                                      fit: BoxFit.fitHeight),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    appName,
+                                    style: TextStyle(
+                                        fontSize: 25, fontWeight: FontWeight.w500),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text("Version $version"),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                        Column(
-                            children: [
-                              Text(appName),
-                              Text("Version $version")
-                            ]
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: sectionMargin),
-                    ),
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              "Entwickler: $developerString \n"
-                          ),
-                          Text(
-                              'Annette App für das Annette-von-Droste-Hülshoff Gymnasium Düsseldorf.' +
+                        Container(
+                          margin: EdgeInsets.only(top: sectionMargin),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                              Text("Entwickler:\n${developers.join(' und ')}\n"),
+                              Text('Annette App für das Annette-von-Droste-Hülshoff Gymnasium Düsseldorf.' +
                                   '\nEine Internet-Verbindung ist für bestimmte Funktionen erforderlich.' +
                                   '\n\nDer Stundenplan wird automatisch bei jedem Neustart der App und beim Ändern der Klasse aktualisiert.' +
                                   '\n\nDer aktuell verwendete Stundenplan ist von Stand:\n${(timeTableVersion != null) ? timeTableVersion.toString() : "Fehler"}' +
                                   '\n\nAlle Angaben ohne Gewähr!' +
-                                  '\n\nwww.annettegymnasium.de' +
-                                  '\n\nKontakt / Feedback: appentwicklung.jan@gmx.de' +
-                                  '\n\n©2021 Jan Wermeckes'
-                          )
-                        ]
-                      )
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: sectionMargin),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        TextButton(
-                          onPressed: () => showLicensePage(context: context),
-                          child: Text("Lizensen anzeigen"),
-                        )
-                      ],
-                    )
-                  ],
-                )
-            )
-        );
+                                  '\n\nKontakt / Feedback:'),
+                              SelectableText(
+                                'appentwicklung.jan@gmx.de',
+                              ),
+                                  Text('\n©2021 ${developers.join(' und ')}'),
 
+                                ])),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: sectionMargin,vertical: sectionMargin),
+                          child: OutlinedButton(
+                              onPressed: () => showLicensePage(
+                                  context: context,
+                                  applicationIcon: Container(
+                                    margin: EdgeInsets.symmetric(vertical: sectionMargin),
+                                    width: 100,
+                                      height: 100,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      shape: BoxShape.rectangle,
+                                      image: DecorationImage(
+                                          image: AssetImage(assetPaths.iconImagePath),
+                                          fit: BoxFit.fitHeight),
+                                    ),
+                                  ),
+                                  applicationName: appName,
+                                  applicationVersion: version),
+                              child: Text("Lizenzen anzeigen"),
+                            ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )));
       },
     );
   }
-  
 }
