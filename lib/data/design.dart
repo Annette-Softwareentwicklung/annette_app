405ed6bb5671590065d25a55427d9b74387edf12
jan2210 <jan.wermeckes@gmx.de>
2022-06-03 16:05:29 +0200

    Farbe Appbar und BottomNavigationBar im Lightmode angepasst

--- lib/data/design.dart
+++ lib/data/design.dart
@@ -4,77 +4,85 @@ import 'package:flutter/material.dart';
 // TODO: implement textThemes
 
 class Design {
+  static Color annetteColor = Color.fromRGBO(0, 136, 148, 1);
   static double standardPagePadding = 10.0;
 
-
   static ThemeData darkTheme = ThemeData(
-      outlinedButtonTheme: OutlinedButtonThemeData(
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor:
+            MaterialStateProperty.resolveWith((states) => Colors.white),
+        textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
+              fontSize: 17,
+            )),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateColor.resolveWith(
+          (states) => annetteColor,
+        ),
+        foregroundColor:
+            MaterialStateColor.resolveWith((states) => Colors.black),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.resolveWith((states) => Colors.white),
-
-          textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
-            fontSize: 17,
-          )),
-
-        ),
+            foregroundColor:
+                MaterialStateProperty.resolveWith((states) => Colors.white))),
+    brightness: Brightness.dark,
+    cupertinoOverrideTheme: CupertinoThemeData(
+      textTheme: CupertinoTextThemeData(
+        dateTimePickerTextStyle: TextStyle(color: Colors.white),
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ButtonStyle(
-          backgroundColor: MaterialStateColor.resolveWith((states) => Color.fromRGBO(0, 136, 148, 1),),
-          foregroundColor: MaterialStateColor.resolveWith((states) => Colors.black),
-        ),
-      ),
-      textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-              foregroundColor:
-              MaterialStateProperty.resolveWith((states) => Colors.white))),
-      brightness: Brightness.dark,
-      cupertinoOverrideTheme: CupertinoThemeData(
-        textTheme: CupertinoTextThemeData(
-          dateTimePickerTextStyle: TextStyle(color: Colors.white),
-        ),
-      ),
-      floatingActionButtonTheme:
-      FloatingActionButtonThemeData(foregroundColor: Colors.black),
-      //buttonTheme: ButtonThemeData(buttonColor: Colors.tealAccent),
-      buttonTheme: ButtonThemeData(buttonColor: Color.fromRGBO(0, 136, 148, 1),),
-      accentColor: Color.fromRGBO(0, 136, 148, 1),
-      snackBarTheme: SnackBarThemeData(
-          contentTextStyle: TextStyle(
-            color: Colors.white,
-          )
-      ),
-    );
+    ),
+    floatingActionButtonTheme:
+        FloatingActionButtonThemeData(foregroundColor: Colors.black),
+    //buttonTheme: ButtonThemeData(buttonColor: Colors.tealAccent),
+    buttonTheme: ButtonThemeData(
+      buttonColor: annetteColor,
+    ),
+    accentColor: annetteColor,
+    snackBarTheme: SnackBarThemeData(
+        contentTextStyle: TextStyle(
+      color: Colors.white,
+    )),
+  );
 
   static ThemeData lightTheme = ThemeData(
-      snackBarTheme: SnackBarThemeData(
-          contentTextStyle: TextStyle(
-            color: Colors.white,
-          )
+    appBarTheme: AppBarTheme(
+      backgroundColor: annetteColor,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: annetteColor,
+    ),
+    snackBarTheme: SnackBarThemeData(
+        contentTextStyle: TextStyle(
+      color: Colors.white,
+    )),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor:
+            MaterialStateProperty.resolveWith((states) => Colors.black87),
+        textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
+              fontSize: 17,
+            )),
       ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: ButtonStyle(
-          foregroundColor: MaterialStateProperty.resolveWith((states) => Colors.black87),
-
-          textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
-            fontSize: 17,
-          )),
-
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateColor.resolveWith(
+          (states) => annetteColor,
         ),
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
+    ),
+    textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateColor.resolveWith((states) => Color.fromRGBO(0, 136, 148, 1),),
-        ),
-      ),
-      textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-              foregroundColor:
-              MaterialStateProperty.resolveWith((states) => Colors.black))),
-      brightness: Brightness.light,
-      accentColor: Colors.black54,
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Color.fromRGBO(0, 136, 148, 1), foregroundColor: Colors.white),
-    );
-
+            foregroundColor:
+                MaterialStateProperty.resolveWith((states) => Colors.black))),
+    brightness: Brightness.light,
+    accentColor: Colors.black54,
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: annetteColor, foregroundColor: Colors.white),
+  );
 }
