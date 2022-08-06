d6d498e3f25f3646b0fec974b04f87c5e848f2ea
Rui Zhang <totallyturing@gmail.com>
2022-08-06 14:18:17 +0200

    Stundenplan-Yoinker Änderungen

--- lib/services/timetable_scraper/timetable_scraper.dart
+++ lib/services/timetable_scraper/timetable_scraper.dart
@@ -4,9 +4,13 @@ import 'package:http/http.dart' as http;
 class TimetableScraper {
   static Future<String> fetch(GroupIDs id) async {
     //TODO: durch production deployment ersetzen
+
+    String date = DateTime.now().toString().substring(0, 10);
+    print(date);
+
     var res = await http.get(Uri.http(
-        'https://annette-entwickelt-software-api.vercel.app/',
-        'api/annette_app/dateien/stundenplan/' + id.name));
+        'annette-entwickelt-software-6oaro0t5x-totallyinformatik.vercel.app',
+        'api/annette_app/dateien/stundenplan/' + id.name + "/" + date));
     print("fetched!");
     if (res.statusCode != 200) {
       print("error error error");
