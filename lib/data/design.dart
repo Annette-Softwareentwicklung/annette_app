5d7bbac9e7c78bcc7c19efe7066161f352dd9282
Rui Zhang <totallyturing@gmail.com>
2022-08-10 17:24:18 +0200

    minor preventative-measures

--- lib/data/design.dart
+++ lib/data/design.dart
@@ -8,45 +8,43 @@ class Design {
   static Color annetteColorLight = Color.fromRGBO(0, 156, 170, 1);
   static double standardPagePadding = 15.0;
   static LinearGradient lightGradient =
-  LinearGradient(colors: [annetteColor, annetteColor]);
+      LinearGradient(colors: [annetteColor, annetteColor]);
   static LinearGradient darkGradient =
-  LinearGradient(colors: [annetteColor, annetteColor]);
+      LinearGradient(colors: [annetteColor, annetteColor]);
 
   static ThemeData darkTheme = ThemeData(
     appBarTheme: AppBarTheme(
       color: Colors.black38,
       foregroundColor: Colors.white,
-      titleTextStyle: TextStyle(
-          fontSize: 20
-      ),
+      titleTextStyle: TextStyle(fontSize: 20),
       // backgroundColor: Colors.black45,
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: ButtonStyle(
         overlayColor: MaterialStateProperty.resolveWith(
-                (states) => annetteColorLight.withOpacity(0.1)),
+            (states) => annetteColorLight.withOpacity(0.1)),
         foregroundColor:
-        MaterialStateProperty.resolveWith((states) => Colors.white),
+            MaterialStateProperty.resolveWith((states) => Colors.white),
         textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
-          fontSize: 17,
-        )),
+              fontSize: 17,
+            )),
       ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         backgroundColor: MaterialStateColor.resolveWith(
-              (states) => annetteColor,
+          (states) => annetteColor,
         ),
         foregroundColor:
-        MaterialStateColor.resolveWith((states) => Colors.black),
+            MaterialStateColor.resolveWith((states) => Colors.black),
       ),
     ),
     textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
             overlayColor: MaterialStateProperty.resolveWith(
-                    (states) => annetteColorLight.withOpacity(0.1)),
+                (states) => annetteColorLight.withOpacity(0.1)),
             foregroundColor:
-            MaterialStateProperty.resolveWith((states) => Colors.white))),
+                MaterialStateProperty.resolveWith((states) => Colors.white))),
     brightness: Brightness.dark,
     cupertinoOverrideTheme: CupertinoThemeData(
       textTheme: CupertinoTextThemeData(
@@ -54,63 +52,55 @@ class Design {
       ),
     ),
     floatingActionButtonTheme:
-    FloatingActionButtonThemeData(foregroundColor: Colors.black),
+        FloatingActionButtonThemeData(foregroundColor: Colors.black),
     buttonTheme: ButtonThemeData(
       buttonColor: annetteColor,
     ),
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-        secondary: annetteColor,
-        brightness: Brightness.dark
-    ),
+    colorScheme: ColorScheme.fromSwatch()
+        .copyWith(secondary: annetteColor, brightness: Brightness.dark),
     snackBarTheme: SnackBarThemeData(
         contentTextStyle: TextStyle(
-          color: Colors.white,
-        )),
+      color: Colors.white,
+    )),
   );
 
   static ThemeData lightTheme = ThemeData(
     appBarTheme: AppBarTheme(
         backgroundColor: Colors.white,
         titleTextStyle: TextStyle(
-            color: annetteColor,
-            fontSize: 20,
-            fontWeight: FontWeight.w600
-        ),
-        iconTheme: IconThemeData(
-            color: annetteColor
-        )
-    ),
+            color: annetteColor, fontSize: 20, fontWeight: FontWeight.w600),
+        iconTheme: IconThemeData(color: annetteColor)),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       selectedItemColor: annetteColor,
     ),
     snackBarTheme: SnackBarThemeData(
         contentTextStyle: TextStyle(
-          color: Colors.white,
-        )),
+      color: Colors.white,
+    )),
     outlinedButtonTheme: OutlinedButtonThemeData(
       style: ButtonStyle(
         overlayColor: MaterialStateProperty.resolveWith(
-                (states) => annetteColorLight.withOpacity(0.1)),
+            (states) => annetteColorLight.withOpacity(0.1)),
         foregroundColor:
-        MaterialStateProperty.resolveWith((states) => Colors.black87),
+            MaterialStateProperty.resolveWith((states) => Colors.black87),
         textStyle: MaterialStateProperty.resolveWith((states) => TextStyle(
-          fontSize: 17,
-        )),
+              fontSize: 17,
+            )),
       ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         backgroundColor: MaterialStateColor.resolveWith(
-              (states) => annetteColor,
+          (states) => annetteColor,
         ),
       ),
     ),
     textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
             overlayColor: MaterialStateProperty.resolveWith(
-                    (states) => annetteColorLight.withOpacity(0.1)),
+                (states) => annetteColorLight.withOpacity(0.1)),
             foregroundColor:
-            MaterialStateProperty.resolveWith((states) => Colors.black))),
+                MaterialStateProperty.resolveWith((states) => Colors.black))),
     brightness: Brightness.light,
     colorScheme: ColorScheme.fromSwatch().copyWith(
       brightness: Brightness.light,
@@ -118,5 +108,6 @@ class Design {
     ),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: annetteColor, foregroundColor: Colors.white),
+    cardColor: Colors.grey[200],
   );
 }
