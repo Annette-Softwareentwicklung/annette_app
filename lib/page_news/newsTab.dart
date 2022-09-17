df6f656307b7e54a9a83793f0e5b36e9295f9981
JonasE22 <101105123+JonasE22@users.noreply.github.com>
2022-09-17 14:32:25 +0200

    Implemented Web View in newsTab

--- lib/page_news/newsTab.dart
+++ lib/page_news/newsTab.dart
@@ -4,20 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+import '../custom_widgets/errorInternetContainer.dart';
+import '../data/links.dart';
 
 ///
 /// Diese Function holt sich die Messages von der API.
 /// Die Nachrichten sind als Markdown Dateien abgespeichert
 /// Bei der API handelt es sich um die private Next.js Annette-App-Api der Annette-Entwickelt-Software AG
 ///
-
-Future<String> getMarkdownData() async {
-
-  http.Response apiResponse = await http.get(Uri.parse("https://annette-entwickelt-software-api.vercel.app/api/annette_app/dateien/nachricht"));
-
-  return apiResponse.body;
-
-}
 
 ///
 /// Der Code für die Ansicht für alle Neuigkeiten
@@ -32,55 +28,61 @@ class NewsTab extends StatefulWidget {
 }
 
 class _NewsTabState extends State<NewsTab> {
+  bool error = false;
 
-  static final String loadingString = "-#.Loading.#-";
-  String markdownNewsData = loadingString;
-
-  initializeMarkdownNewsData() async {
-
-    String response = await getMarkdownData();
-    setState(() {
-      markdownNewsData = response;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    initializeMarkdownNewsData();
+  void showError() {
+    final snackBar = SnackBar(
+      duration: Duration(seconds: 3),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.warning_rounded,
+            color: Colors.white,
+          ),
+          Container(
+            child: Text('Laden fehlgeschlagen', style: TextStyle(fontSize: 17)),
+            margin: EdgeInsets.only(left: 15),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.redAccent,
+      margin: EdgeInsets.all(10),
+      behavior: SnackBarBehavior.floating,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
   Widget build(BuildContext context) {
 
-    if (markdownNewsData == loadingString) { /// Inhalt ist noch nicht geladen worden
-      return CupertinoActivityIndicator();
+    if (error) {
+      return ErrorInternetContainer(
+        onRefresh: () {
+          setState(() {
+            error = false;
+          });
+        },
+      );
     }
 
     return SafeArea(
         child: RefreshIndicator(
           onRefresh: () async {
-            String response = await getMarkdownData();
-            setState(() {
-              markdownNewsData = response;
-            });
+
           },
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(Design.standardPagePadding),
-              child: MarkdownBody(
-                onTapLink: (String string1, String? url, String string3) {
-                  if (url == null) return;
-
-                  launchUrlString(url);
-
-                },
-                data: markdownNewsData
-              )
+            child: WebView(
+                initialUrl:
+                'https://www.annettegymnasium.de/',
+                javascriptMode: JavascriptMode.unrestricted,
+                onProgress: (progress) => CupertinoActivityIndicator(),
+                onWebResourceError: (e) {
+                  setState(() {
+                    showError();
+                    error = true;
+                  });
+                }
             )
-          ),
-
         )
     );
   }
